47489a06a2b4614339d31e4418ecf44d790b763b
unknown <631320019@SALA602-05.fspoaeduc.com.br>
2015-04-08 11:20:05 -0300

    Criando metódos da lista

--- br.com.senacrs.listaduplaencadeada/src/main/List.java
+++ br.com.senacrs.listaduplaencadeada/src/main/List.java
@@ -1,5 +1,32 @@
 package main;
+import main.Node;
 
-public class List {
+public class List<T> {
+	
+	private Node<T> first, last;
+	private int size;
+	
+	public List () {
+		
+	}
+	
+	public void isEmpty () {
+		
+	}
+	
+	public void addElement (Node<T> newNode, Node<T> nextNode) {
+		Node<T> Node = new Node<T> ();
+		Node.setNext(nextNode);
+		Node.setPrev(nextNode.getPrev());
+		nextNode.setPrev(Node);
+		
+	}
+	
+	public void removeElement (Node<T> nodeToBeRemoved) {
+		
+		
+	}
+	
+	
 
 }

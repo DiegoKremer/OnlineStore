90407c15dc7f067117167c04f12b0fcd8eb8729b
DiegoKremer <diego.kremer@gmail.com>
2015-04-25 12:49:12 -0300

    #1

---
+++ br.com.senacrs.onlinestore/src/structure/DoublyLinkedList.java
@@ -0,0 +1,20 @@
+package structure;
+
+public class DoublyLinkedList<T> {
+
+	public static class Node<T> {
+
+		private T data;
+		private Node next;
+		private Node previous;
+
+		public Node (T data) {
+			
+			this.data = data;
+
+		}
+	
+	}
+
+
+}

342a3c4195ddebb4e6df9bf14b753b2579124345
DiegoKremer <diego.kremer@gmail.com>
2015-05-17 14:50:10 -0300

    New Cart methods and comments

--- br.com.senacrs.onlinestore/src/model/Cart.java
+++ br.com.senacrs.onlinestore/src/model/Cart.java
@@ -1,6 +1,9 @@
 package model;
 import java.util.Vector;
 
+/* Creates a class representing an Item of the cart. 
+ * It consists of an Item and its amount. */
+
 class ItemCart {
 	
 	public Item item;
@@ -13,6 +16,9 @@ class ItemCart {
 		
 	}
 }
+
+
+/* The Cart class */
 
 public class Cart {
 	
@@ -42,6 +48,8 @@ public class Cart {
 		return itemCount;
 	}
 	
+	/* Gets the total price of all the items 
+	 * in the cart. */
 	
 	public double getTotal () {
 		double total = 0.0;
@@ -52,6 +60,11 @@ public class Cart {
 		return total;
 	}
 	
+	/* Loop through the item array, if the element is
+	 * null it will break, otherwise it compares the 
+	 * item parameter with the current element of the 
+	 * array and if it is equal it will increment the
+	 * amount. */
 	
 	public void addItem (Item item, int amount) {	
 		for (ItemCart e : items)
@@ -62,12 +75,19 @@ public class Cart {
 				return;
 			}
 		}
+		
+		/* If the items in the cart are equal to the 
+		 * array length, it will increase the size of
+		 * the cart and add the item as a new ItemCart. */
+		
 		if (itemCount == items.length)
 			increaseCart();
 		items[itemCount] = new ItemCart(item, amount);
 		itemCount++;
 	}
 
+	/* Will increase the size of the cart by creating a copy
+	 * twice as bigger. */
 
 	private void increaseCart() {
 		ItemCart[] newItemCart = new ItemCart[items.length * 2];
@@ -75,6 +95,22 @@ public class Cart {
 		items = newItemCart;
 	
 	}
+	
+	
+	/* Will loop through the array comparing the Item parameter,
+	 * if it finds and item equal will remove it */
+	
+	public void removeItem(Item item) {
+		for (int i = 0; i < itemCount; ++i) {
+			if (items[i].item.compareTo(item) == 0) {
+				items[i] = items[itemCount-1];
+				items[itemCount-1] = null;
+				itemCount--;
+				return;
+			}
+		}
+	}
+	
 	
 	
 	

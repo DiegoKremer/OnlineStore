bbdc125c59afdaeb2891d0e95e56b9ad06148a2a
Diego Kremer <diego.kremer@gmail.com>
2015-05-27 11:10:51 -0300

    Starting HashTable implementation

---
+++ br.com.senacrs.onlinestore/src/structure/HashTable.java
@@ -0,0 +1,17 @@
+package structure;
+
+import model.Book;
+
+public class HashTable<Book> {
+	
+	private int initialCapacity;
+	
+	public HashTable (int initialCapacity) {
+		this.initialCapacity = initialCapacity;
+	}
+	
+	public void insert (Book book) {
+		
+	}
+
+}
